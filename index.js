a2efa0b2e7a35da14ae914d85c2aa9eb732da5e6
Wade <wsw0615@gmail.com>
2020-01-01 18:10:24 +0800

    feat: function setGameOver

--- index.js
+++ index.js
@@ -41,3 +41,12 @@ function checkGuess() {
 }
 
 guessSubmit.addEventListener('click', checkGuess);
+
+function setGameOver() {
+    guessField.disabled = true;
+    guessSubmit.disabled = true;
+    resetButton = document.createElement('button');
+    resetButton.textContent = 'Start new game';
+    document.body.appendChild(resetButton);
+    resetButton.addEventListener('click', resetGame);
+}
